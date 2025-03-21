8ac8f9f18b338aa0a31dffa4c2cd50d1f5f4bcfa
IhorMuliar <goga440@gmail.com>
2025-03-22 00:11:54 +0200

    Minor refactoring

--- services/rss_service.py
+++ services/rss_service.py
@@ -1,8 +1,15 @@
 import asyncio
+import os
 import feedparser
 from telegram import Bot
+from dotenv import load_dotenv
 
-CHAT_ID = '141279468'
+load_dotenv(".env")
+chat_id = os.getenv("CHAT_ID")
+
+if not chat_id:
+    raise ValueError("Chat ID not found. Please check your .env file.")
+
 RSS_URL = 'https://djinni.co/jobs/rss/?primary_keyword='
 DOU_RSS_URL = 'https://jobs.dou.ua/vacancies/feeds/?search=angular&category=Front%20End'
 
@@ -20,14 +27,14 @@ async def fetch_feed_updates(bot: Bot, category: str):
             latest_title = djinni_feed.entries[0].title
             if latest_title != previous_titles["djinni"]:
                 message = f"New job posted on djinni:\n{latest_title}\n{djinni_feed.entries[0].link}"
-                await bot.send_message(chat_id=CHAT_ID, text=message)
+                await bot.send_message(chat_id=chat_id, text=message)
                 previous_titles["djinni"] = latest_title
 
         if dou_feed.entries:
             latest_title = dou_feed.entries[0].title
             if latest_title != previous_titles["dou"]:
                 message = f"New job posted on dou:\n{latest_title}\n{dou_feed.entries[0].link}"
-                await bot.send_message(chat_id=CHAT_ID, text=message)
+                await bot.send_message(chat_id=chat_id, text=message)
                 previous_titles["dou"] = latest_title
     except Exception as e:
         print(f"Error fetching feeds: {e}")
